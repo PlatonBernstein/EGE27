155fb0304a72928b2de4e2db08d1070461dc86ac
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 01:04:30 +0300

    plan created

---
+++ number_sums_div_eff_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/*
+1. Input sequence length and sequence
+2. Calculate maximal odd and even numbers
+3. Calculate their sum and print it
+*/
+
+const int maxabs = 1000;
+
+int main()
+{
+    return 0;
+}
