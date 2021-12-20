7bd6a06e724f2797c197f370f20dd1d72f49f7d7
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 00:49:59 +0300

    plan created

---
+++ number_products_div_eff_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/*
+1. Input array size and numbers
+2. How many numbers divide by 14?
+3. How many numbers divide by 2, but not by 7?
+4. How many numbers divide by 7, but not by 2?
+5. Calculate total count
+*/
+
+int main()
+{
+    return 0;
+}
