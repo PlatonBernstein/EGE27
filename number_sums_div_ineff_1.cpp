12d2a79cc02ae5ebc735ea98da35657a3c024ac7
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 01:03:27 +0300

    Input array size and content

--- number_sums_div_ineff_1.cpp
+++ number_sums_div_ineff_1.cpp
@@ -8,5 +8,15 @@ using namespace std;
 
 int main()
 {
+    int n, checksum = -1;
+    int i, j, s;
+    bool checksum_found = false;
+    // Ввод размера массива
+    std::cin >> n;
+    int* x = new int[n];
+    // Ввод всех х
+    for (i = 0; i < n; i++) {
+        std::cin >> x[i];
+    }
     return 0;
 }
