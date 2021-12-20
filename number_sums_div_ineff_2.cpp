bd5a5a206aea6824bdba608fb6461f160b0b5b3a
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 01:07:05 +0300

    plan created and constants added

---
+++ number_sums_div_ineff_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/*
+1. Input array size and content
+2. Calculate checksum
+*/
+
+const int divider = 40;
+const int maxx = 40;
+
+int main()
+{
+    return 0;
+}
