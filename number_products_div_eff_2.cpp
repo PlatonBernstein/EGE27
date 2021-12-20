5a983f4eb9de7fd2bc4d1f35f25eca2f897de6ed
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 00:58:38 +0300

    plan created

---
+++ number_products_div_eff_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/*
+1. Input array size and numbers
+2. How many numbers divide by 105?
+3. Make a function to calculate count - how many divide by num1 and num2, but not by num3
+4. How many numbers divide by 5*7, but not 3; by 3*7, but not 5; by 3*5, but not 7
+5. Make a function to calculate count - how many divide by num1 and num2, but not by num3, OR divide by num2 and num3, but not num1
+6. How many numbers divide by 3*5 and 5*7; by 3*7 and 7*5; by 3*5 and 5*7
+*/
+
+
+int main()
+{
+    return 0;
+}
