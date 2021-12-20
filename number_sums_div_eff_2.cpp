c90423080a3beae967e26bc7cbea1036dee0f74b
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 01:08:30 +0300

    plan created and constants added

---
+++ number_sums_div_eff_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+1. Input sequence, till -9999 found
+2. Calculate pairs for numbers greater than 40
+3. Calcualte pairs for other numbers
+4. Calculate pairs for numbers, which divide by 40
+*/
+
+const long divider = 40;
+const long maxx = 40;
+
+
+int main()
+{
+    return 0;
+}
