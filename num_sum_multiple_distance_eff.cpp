1c0b93455f8c9aa6de18f8a9d16ba5d79ff2c2d2
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 01:25:18 +0300

    Input sequence

--- num_sum_multiple_distance_eff.cpp
+++ num_sum_multiple_distance_eff.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 /*
-1. Input sequence length
+1. Input sequence
 2. Calculate pairs for each of 4 slices/streams, having different modulus
 3. Add number of pairs for each of 4 slides/streams, having same modulus
 */
@@ -12,5 +12,22 @@ const long dist = 4;
 
 int main()
 {
-    return 0;
+    long long n;
+    long long count_total = 0;
+    long i, j, t;
+    long long** entered_quantity = new long long* [dist]; // Сколько у нас возможно "линий" в вводимых данных
+    for (i = 0; i < dist; i++) {
+        entered_quantity[i] = new long long[divider]; // Сколько введено чисел с таким вот остатком от деления
+        for (j = 0; j < divider; j++) {
+            entered_quantity[i][j] = 0;
+        }
+    }
+    // Ввод размера массива
+    cin >> n;
+    // Ввод очередного x
+    for (i = 0; i < n; i++) {
+        cin >> t;
+        t = t % divider;
+        entered_quantity[i % dist][t]++;  // накрутили счётчик остатков от деления
+    }
 }
