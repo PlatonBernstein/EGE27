7ddd68456b30e3c8f81f9c8c490beeebab73ca11
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 01:24:29 +0300

    plan created and constants added

---
+++ num_sum_multiple_distance_eff.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/*
+1. Input sequence length
+2. Calculate pairs for each of 4 slices/streams, having different modulus
+3. Add number of pairs for each of 4 slides/streams, having same modulus
+*/
+
+const long divider = 39;
+const long dist = 4;
+
+int main()
+{
+    return 0;
+}
