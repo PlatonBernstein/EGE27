6fef09ac0ebdd82d41c17a44c95f7a4331455a2e
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 01:19:51 +0300

    Calculate minsum

--- min_sum_min_distance_ineff.cpp
+++ min_sum_min_distance_ineff.cpp
@@ -15,5 +15,18 @@ int main()
     cin >> n;
     minsum = 1000;
     long long* x = new long long[n];
+    // Ввод всех х
+    for (i = 0; i < n; i++) {
+        cin >> x[i];
+    }
+    // Производим вычисления
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (abs(i - j) >= 4 && x[i] + x[j] < minsum) {
+                minsum = x[i] + x[j];
+            }
+        }
+    }
+    cout << minsum;
     return 0;
 }
