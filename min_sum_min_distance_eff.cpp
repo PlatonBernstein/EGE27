031a24b46edc90016c6dc861227e4c62bb87aa96
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 01:20:38 +0300

    plan created

---
+++ min_sum_min_distance_eff.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/*
+1. Input array size
+2. Fill buffer - input first 4 numbers
+3. Move buffer
+4. Calculate sums
+*/
+
+int main()
+{
+    return 0;
+}
