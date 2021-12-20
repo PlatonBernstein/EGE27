f2928b3a69cf1500205db322677d3a63dbe2c780
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 00:46:41 +0300

    Input array size and content

--- number_products_div_ineff_1.cpp
+++ number_products_div_ineff_1.cpp
@@ -7,5 +7,15 @@
 
 int main()
 {
+    int n, count;
+    int i, j;
+    // Ввод размера массива
+    std::cin >> n;
+    count = 0;
+    int* x = new int[n];
+    // Ввод всех х
+    for (i = 0; i < n; i++) {
+        std::cin >> x[i];
+    }
     return 0;
 }
