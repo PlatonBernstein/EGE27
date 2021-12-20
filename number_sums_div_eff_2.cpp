c08c16eea1910bf404f430c1806f18c446f428ee
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 01:09:16 +0300

    Input sequence, till -9999 found

--- number_sums_div_eff_2.cpp
+++ number_sums_div_eff_2.cpp
@@ -14,5 +14,31 @@ const long maxx = 40;
 
 int main()
 {
+    long long t;
+    long long count_total = 0, count_g_g = 0, count_g_le = 0, count_le_g = 0;
+    long i, j;
+    long long* entered_quantity_g = new long long [divider]; // Сколько введено чисел с таким остатом от деления, и больше 40
+    for (j = 0; j < divider; j++) {
+        entered_quantity_g[j] = 0;
+    }
+    long long* entered_quantity_le = new long long[divider]; // Сколько введено чисел с таким остатком от деления, и не больше 40
+    for (j = 0; j < divider; j++) {
+        entered_quantity_le[j] = 0;
+    }
+    // Ввод очередного x
+    for(;;) {
+        cin >> t;
+        if (t == -9999) {
+            break;
+        }
+        bool g = (t > maxx);
+        t = t % divider;
+        if (g) {
+            entered_quantity_g[t] ++;
+        }
+        else {
+            entered_quantity_le[t] ++;
+        }
+    }
     return 0;
 }
