b7fac8ce1eab39ffe421690f9e1f479da8b70ebb
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 01:16:50 +0300

    plan created

---
+++ num_sum_max_distance_eff.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/*
+1. Input array size and content
+2. Fill buffer - input first 6 numbers
+3. Move buffer & calculate
+4. Calculate in reminder of buffer - last 5 pairs
+*/
+
+int main()
+{
+    return 0;
+}
