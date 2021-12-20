9899e333107865d2261947b9e3b3f27d088ebd3e
Платон Бернштейн <platon.bernstein@mail.ru>
2021-12-21 00:59:15 +0300

    Input array size and numbers

--- number_products_div_eff_2.cpp
+++ number_products_div_eff_2.cpp
@@ -12,5 +12,14 @@
 
 int main()
 {
-    return 0;
+    int n, count;
+    int i;
+    // Ввод размера массива
+    std::cin >> n;
+    count = 0;
+    int* x = new int[n];
+    // Ввод всех х
+    for (i = 0; i < n; i++) {
+        std::cin >> x[i];
+    }
 }
